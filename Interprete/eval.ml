b8a96f3a21dcaa4fc9b1ea5bccfb73eb301d05af
JackJox26 <115089580+JackJox26@users.noreply.github.com>
2023-01-04 15:25:53 +0100

    Create eval.ml
    test

---
+++ Interprete/eval.ml
@@ -0,0 +1,33 @@
+%{
+  open Ast
+%}
+
+
+%token <string> ID
+%token <int> CSTE
+%token AFFECT
+%token PARENT_G PARENT_D POINTVIRGULE DEUXPOINTS POINT
+%token IF THEN ELSE 
+%token ACCOLADE_G ACCOLADE_D
+%token IS VAR EXTENDS
+%token AUTO OBJECT DEF NEW
+%token PLUS MOINS MUL DIV UMOINS
+%token PG PGE PP PPE EGAL NEGAL (*(OPERATEUR)*)
+%token CONCAT
+%token EOF
+
+%start<Ast.progType> prog
+%%
+verifie si l'expression renvoie pas 
+let surcharge e lmethods =
+  let rec sur_aux e lmethods classTemp res=
+    match e with 
+      Object x ->
+        match x 
+          
+        else lmethods = 
+  in r
+
+type grid = int list list
+let methodClass e =
+    let me
