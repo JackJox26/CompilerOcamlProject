b6ee9605ceab25a36ae446527eae4830d388d2ea
g-micha <78092855+g-micha@users.noreply.github.com>
2023-01-09 15:57:12 +0100

    update ast.ml

--- ast.ml
+++ ast.ml
@@ -22,65 +22,71 @@ type typeType = Type of string
 
 type declType = Decl of (string list * typeType)
 
+(* type cibleType = Result | *) 
+
 type instructionType = 
 	(*Exp of expType
 	| *) Bloc of blocType 
 	(*| IfThenElse of (expType*instructionType*instructionType) *) 
 	| Return
+	(*| Affectation of (cibleType * expType)*) 
 
 and
 blocType = 
 	BlocLInst of instructionType list 
-	| BlocDecl of (declType list * instructionType list) 
+	| BlocDecl of (declType list * instructionType list)
+	| Empty
 
 
 
 (*
-type paramType = Param of (boolean * string * typeType)
+type paramType = Param of (string * typeType)
  *)
 
  (*
- type methodeCorps = Val of (typeType * expType) | ResultType of (typeType * blocType) | ResultSimple of blocType
+ type champsType = Champs of (boolean * paramType)
  *)
 
-  (*
+ (*
+ type methodeCorps = Val of (typeType * expType) 
+ 	| ResultType of (typeType * blocType) 
+	| ResultSimple of blocType
+ *)
+
+(*
  type methodeType = {
 	nom : string;
 	listParam : paramType list;
 	typeRetour : typeType option;
 	isOverride : boolean;
 	corps : blocType;
-	methodeCorps : methodeCorps;
+	methodeCorps : methodeCorps
  }
  *)
 
 (*
-type corpsType = Corps of (	paramType list * methodeType list)
+type corpsType = Corps of (	champsType list * methodeType list)
 *)
 
 (*
-type heritageType = 
-{
-	nom : string;
-	listArgs : string list;
-}
+type heritageType = Heritage of { nom : string; listArgs : string list } | EmptyHeritage
 *)
 
 (*
 type classeType = 
 {	nom : string;
 	listParam : paramType list;
-	OHeritage : heritageType option;
-	OConstruct : blocType option;
-	corps : corpsType;
+	oHeritage : heritageType;
+	oConstruct : blocType;
+	corps : corpsType
 }
 *)
 
 (*
 type objetIsoleType = 
 {	nom : string;
-	OConstruct : blocType option;
-	corps : corpsType;
+	oConstruct : blocType;
+	corps : corpsType
 }
 *)
 
