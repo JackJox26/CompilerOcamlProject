0487df1f22625701fc0c0c8aab7a117a6dfcb07b
TheoDELORME <theodelorme@gmail.com>
2023-01-09 15:59:32 +0100

    Update Gabrielle

--- ast.ml
+++ ast.ml
@@ -6,7 +6,7 @@ type opType =
     |EGAL
     |NEGAL
 
-
+(*
 type expType =
 	Id of string
 	|Cste of int
@@ -17,70 +17,76 @@ type expType =
 	|Div of (expType * expType)
 	|PlusU of expType
 	|MoinsU of expType
-
+*)
 type typeType = Type of string
 
 type declType = Decl of (string list * typeType)
+
+(* type cibleType = Result | *) 
 
 type instructionType = 
 	(*Exp of expType
 	| *) Bloc of blocType 
 	(*| IfThenElse of (expType*instructionType*instructionType) *) 
 	| Return
+	(*| Affectation of (cibleType * expType)*) 
 
 and
 blocType = 
 	BlocLInst of instructionType list 
-	| BlocDecl of (declType list * instructionType list) 
+	| BlocDecl of (declType list * instructionType list)
+	| Empty
 
 
 
 (*
-type paramType = Param of (boolean * string * typeType)
+type paramType = Param of (string * typeType)
  *)
 
  (*
- type methodeCorps = Val of (typeType * expType) | ResultType of (typeType * blocType) | ResultSimple of blocType
+ type champsType = Champs of (boolean * paramType)
  *)
 
-  (*
+ (*
+ type methodeCorps = Val of (typeType * expType) 
+ 	| ResultType of (typeType * blocType) 
+	| ResultSimple of blocType
+ *)
+
+(*
  type methodeType = {
 	nom : string;
 	listParam : paramType list;
 	typeRetour : typeType option;
 	isOverride : boolean;
 	corps : blocType;
-	methodeCorps : methodeCorps;
+	methodeCorps : methodeCorps
  }
  *)
 
 (*
-type corpsType = Corps of (	paramType list * methodeType list)
+type corpsType = Corps of (	champsType list * methodeType list)
 *)
 
 (*
-type heritageType = 
-{
-	nom : string;
-	listArgs : string list;
-}
+type heritageType = Heritage of { nom : string; listArgs : string list } | EmptyHeritage
 *)
 
 (*
 type classeType = 
 {	nom : string;
 	listParam : paramType list;
-	oHeritage : heritageType option;
-	oConstruct : blocType option;
-	corps : corpsType;
+	oHeritage : heritageType;
+	oConstruct : blocType;
+	corps : corpsType
 }
 *)
 
 (*
 type objetIsoleType = 
 {	nom : string;
-	oConstruct : blocType option;
-	corps : corpsType;
+	oConstruct : blocType;
+	corps : corpsType
 }
 *)
 
