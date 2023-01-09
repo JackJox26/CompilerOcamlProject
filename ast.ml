fcfd0a35ef7d0454e55f11a6d0e99745ef12df0d
Waagheur <natgabrib@outlook.fr>
2023-01-09 13:30:53 +0100

    Update ast.ml

--- ast.ml
+++ ast.ml
@@ -1,59 +1,25 @@
-type op
+type opComp =
+  Eq | Neq | Lt | Le | Gt | Ge
 
-(*
 type expType =
-	Id of string
-	|Cste of int
-	|Parent of expType
-	|Plus of (expType * expType)
-	|Moins of (expType * expType)
-	|Mult of (expType * expType)
-	|Div of (expType * expType)
-	|PlusU of expType
-	|MoinsU of expType
-*)
-type typeType = Type of string
+  Id of string
+| Cste of int
+| Plus of expType*expType
+| Minus of expType*expType
+| Times of expType*expType
+| Div of expType*expType
+| Uminus of expType
+| Ifel of compType*expType*expType
 
-type declType = Decl of (string list * typeType)
+and compType =
+  Comp of expType*opComp*expType
 
-type instructionType = 
-	(*Exp of expType
-	| *) Bloc of blocType 
-	(*| IfThenElse of (expType*instructionType*instructionType) *) 
-	| Return
+type declarationType = string*expType
 
-and
-blocType = 
-	BlocLInst of instructionType list 
-	| BlocDecl of (declType list * instructionType list) 
+type declarationsType = declarationType list
 
-type objetType (*= 
-	Classe of (string*paramType list*
-	ObjetIsole of
-	
-type classeType = 
-{	nom : string;
-	listParam : (string*string) list
-	OHeritage :
-	OConstruct :
-	corp : 
-}
+type progType = declarationsType*expType
 
-type paramType = Param of (string * string)
- *)
-
-type progType = Prog of (objetType list * blocType)
-	
-(* 
-type membreType = Attribut of (expType*string)
-
-type cibleType = 
-	|Result
-	|Var of string
-	|Membre of membreType
-
-type objetIsole = 
-
-type objetType = classe | objetIsole 
-
-*)
+exception VC_Error of string
+exception RUN_Error of string
+exception MISC_Error of string
